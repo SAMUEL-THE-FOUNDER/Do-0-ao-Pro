e30c59f912f855d31291d7ce17abd64d1a808b89
SAMUEL-THE-FOUNDER <samuelcremonezi2703@gmail.com>
2024-05-24 19:06:46 -0300

    não tinha ido td antes kk

--- modulo 3/Desafio_trocaDeVal.js
+++ modulo 3/Desafio_trocaDeVal.js
@@ -1,13 +1,27 @@
+let a = 7;
+let b = 94;
 
+// a = 7 + 87;
+// b = a - 87;
+
+// console.log(a);
+// console.log(b);
 
 // ouuuu
 
-let a = 7
-let b = 94
-let ab = a
+//  let a = 7;
+//  let b = 94;
+//  let ab = a;
 
-a = b
-b = ab
+//  a = b;
+//  b = ab;
  
-console.log(a)
-console.log(b)
+//  console.log(a);
+//  console.log(b);
+
+// ouuuuuuuuuuuu 
+
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
