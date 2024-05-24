c672c44a3855eae6a5d5b6426ce057b4bcc723b2
SAMUEL-THE-FOUNDER <samuelcremonezi2703@gmail.com>
2024-05-24 16:02:13 -0300

    teste

--- Dados_3.js
+++ Dados_3.js
@@ -1,17 +1,2 @@
-let preco = 19.90;
-let desconto = 0.4;
-
-console.log(preco * (1 - desconto));
-
-let precoComDesconto = preco * (1- desconto);
-console.log(precoComDesconto)
-
-
-let nome = "caderno";
-let categoria = "papelaria";
-console.log("produto: " + nome 
-    + ", categoria: " + categoria
-    + ", preço: " + preco 
-    + ", desconto: " + desconto
-    + ", preço com desconto: " + precoComDesconto
-);
+// string = texto
+//asasasasasasasa
