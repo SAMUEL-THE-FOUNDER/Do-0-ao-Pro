ed9bc698975d020cbc902096fe89a3efaf08b081
SAMUEL-THE-FOUNDER <samuelcremonezi2703@gmail.com>
2024-05-24 18:41:18 -0300

    finalizei o modulo 3

---
+++ Desafio_area.js
@@ -0,0 +1,11 @@
+//pi * raio * raio
+//PI = 3.141592
+//quando for algo constante ja criar com letra maiuscula 
+
+const PI = 3.141592;
+const raio = 50;
+let area = PI * raio * raio
+console.log("a area é: " + area + "m2.")
+
+area = Math.PI * raio * raio
+console.log("a area é: " + area + "m2.")
---
+++ Desafio_trocaDeVal.js
@@ -0,0 +1,13 @@
+
+
+// ouuuu
+
+let a = 7
+let b = 94
+let ab = a
+
+a = b
+b = ab
+ 
+console.log(a)
+console.log(b)
